49d789b7b98cab725ee181dc63642aff39f7659d
Rania_Elbalq <92025169+blq200@users.noreply.github.com>
2022-06-18 23:12:05 +0200

    Add files via upload

---
+++ chefchaouen/chefchaouen.php
@@ -0,0 +1,35 @@
+<?php include('navbar.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!---------------------Fontawsome------------------------>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!---------------------Bootstrap------------------------>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     <!---------------Font-Family----------->
+    <link href="https://fonts.googleapis.com/css2?family=Single+Day&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet"></head>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Signika+Negative:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Simonetta&display=swap" rel="stylesheet">
+    <title>Chefchaouen</title>
+</head>
+
+<body>
+    <header>
+<img src="headerchef.png" class="img-fluid" style="width:100%; background-size:cover;height:23em;" alt="...">
+
+<div class="div" style=" display: flex; flex-direction: row;justify-content: center;">
+        <h1 style=" color: black; font-family: single day; font-size:3em; ">OPENING DOORS</h1>
+    </div> 
+    <h3  class="subtitle"style="text-align:center;">Find Your Next Exchange</h3>
+    </header>
+</body>
+</html>
+
